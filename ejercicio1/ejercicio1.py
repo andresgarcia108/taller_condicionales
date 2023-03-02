1bf5d2ff628514fd291e2bcb4f3fc9f6b8cd7146
andresgarcia108 <andresgarciia@colegioguanenta.edu.co>
2023-03-02 10:55:23 -0500

    Se actualizó el código

--- ejercicio1/ejercicio1.py
+++ ejercicio1/ejercicio1.py
@@ -1,9 +1,9 @@
 # Programa para leer cordenadas de un plano cartesiano e indicar el cuadrante
 
 
-print ("---------------------------------------------------")
-print ("Programa para indicar el cuadrante unas coordenadas")
-print ("---------------------------------------------------")
+print("\x1b[;31m"+"-------------------------------------------------------"+'\033[0;m')
+print("\033[4;35m"+"Programa para indicar el cuadrante de  unas coordenadas"+'\033[0;m')
+print("\x1b[;31m"+"-------------------------------------------------------"+'\033[0;m')
 
 #input
 
@@ -14,23 +14,23 @@ y = int(input("Digite la coordenada y: "))
 
 if x == 0:
     if y == 0:
-        msj =("Las coordenadas corresponden al origen")
+        msj = ("Las coordenadas corresponden al origen")
     else:
-        msj =("Las  coordenadas corresponden al eje y")
+        msj = ("Las coordenadas corresponden al eje y")
 else:
     if y == 0:
-        msj =("Las coordenadas corresponden al eje x")
+        msj = ("Las coordenadas corresponden al eje x")
+    else:
         if x > 0:
             if y > 0:
-                  msj =("Las coordenadas corresponden al cuadrante 1")
-        else :
-            msj =("Las coordenadas corresponden al cuadrante 4")
-    else:
-        if y < 0:
-            msj =("Las coordenadas corresponden al cuadrante 3")
+                msj = ("Las coordenadas corresponden al cuadrante 1")
+            else:
+                msj = ("Las coordenadas corresponden al cuadrante 4")
         else:
-            msj =("Las coordenadas corresponden al cuadrante 2")
-
+            if y < 0:
+                msj = ("Las coordenadas corresponden al cuadrante 3")
+            else:
+                msj = ("Las coordenadas corresponden al cuadrante 2")
 # output
-
 print (msj)
+        
