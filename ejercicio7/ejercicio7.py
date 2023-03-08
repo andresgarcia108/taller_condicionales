80a179cc1810af7fb6aea6004a381364e584354b
andresgarcia108 <andresgarciia@colegioguanenta.edu.co>
2023-03-07 21:36:53 -0500

    Se agregó el diagrama y el código

---
+++ ejercicio7/ejercicio7.py
@@ -0,0 +1,18 @@
+# input
+
+a = int(input("Digite el primer numero entero: "))
+b = int(input("Digite el segundo numero entero: "))
+c = int(input("Digite el tercer numero entero: "))
+
+#processing
+
+if a + b == c:
+    msj = ("La suma de los dos primeros números es igual al tercero")
+else:
+    msj = ("La suma de los dos primeros números no es igual al tercero")
+#output
+print (msj)
+
+
+
+
