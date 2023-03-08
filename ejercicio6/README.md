68747deea927d41d34b9e0be74cf9c9e957d9e0d
andresgarcia108 <andresgarcia@colegioguanenta.edu.co>
2023-03-08 09:15:42 -0500

    Se agregó el código y el README

---
+++ ejercicio6/README.md
@@ -0,0 +1,4 @@
+Programa para resolver una ecuación cuadrática de segundo grado
+
+# Diagrama de flujo
+![Diagrama de flujo](diagrama.png "Diagrama de flujo")
---
+++ ejercicio6/ejercicio6.py
@@ -0,0 +1,26 @@
+#Programa para resolver una ecuación cuadrática de segundo grado
+import math
+
+print ("--------------------------------------")
+print ("-----Resolver ecuación caudrática-----")
+print ("--------------------------------------")
+
+#input
+
+a = int(input("Digite el valor del coeficiente a:"))
+b = int(input("Digite el valor del coeficiente b:"))
+c = int(input("Digite el valor del coeficiente c:"))
+
+#processing
+
+d = (b**2 - 4*a*c)
+if d == 0:
+    x = (-b/2*a)
+    x2 = x
+    print (x, x2)
+if d > 0:           
+    x = (-b + math.sqrt(d))/(2*a)
+    x2 = (-b-math.sqrt(d))/(2*a)
+    print (x, x2)
+else:
+    print ("La solución es imaginaria")
