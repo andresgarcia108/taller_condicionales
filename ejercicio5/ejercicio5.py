acbf1eb9ea36d1fa9e515b312457fcfdd7d6fea7
andresgarcia108 <andresgarcia@colegioguanenta.edu.co>
2023-03-09 15:25:51 -0500

    Se modificó el código

--- ejercicio5/ejercicio5.py
+++ ejercicio5/ejercicio5.py
@@ -5,22 +5,22 @@ print ("-------Calcular el gasto del agua-------")
 print ("----------------------------------------")
 
 # input
-gasto  = int(input("Digite el gasto del agua en metros cuadrados: "))
+gasto  = int(input("Digite el gasto del agua en metros cubicos: "))
 
 # processing
 
 if gasto < 50:
     precio = 10000
-    msj = ("El precio del agua es: ", (precio))
+    msj = "El precio del agua es: ", (precio)
 else:
     if gasto < 200:
          precio = (gasto - 50) *2000 + 10000
-         msj = ("El precio del agua es: ", (precio))
+         msj = "El precio del agua es: ", (precio)
     else:
         if gasto >200:
                 precio = (gasto - 50) *3000 +10000
-                msj = ("El precio del agua es: ", (precio))
+                msj = "El precio del agua es: ", (precio)
         else:
             precio = (gasto - 50) *2000 + 10000
-            msj = ("El precio del agua es: ", (precio))
+            msj = "El precio del agua es: " + (precio)
 print (msj)
